cc2550fcead1b45748f0bc8ab5f5b41afc348aff
DDefty <dawid.kogut99@gmail.com>
2025-09-22 12:41:37 +0200

    Implement state transformation logic in transformStateWithClones function

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arrOfStates = [];
+  const tempState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(tempState, action.extraData);
+      arrOfStates.push({ ...tempState });
+    } else if (action.type === 'removeProperties') {
+      for (const keys of action.keysToRemove) {
+        delete tempState[keys];
+      }
+      arrOfStates.push({ ...tempState });
+    } else if (action.type === 'clear') {
+      for (const key in tempState) {
+        delete tempState[key];
+      }
+      arrOfStates.push({ ...tempState });
+    }
+  }
+
+  return arrOfStates;
 }
 
 module.exports = transformStateWithClones;
